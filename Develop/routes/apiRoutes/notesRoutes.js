e3327e02da262ab49ea6446b162ca93e075e5c09
Cody Callahan <codycallahan02@gmail.com>
2021-09-02 11:30:05 -0700

    fix issue with starting db file

--- Develop/routes/apiRoutes/notesRoutes.js
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {createNote} = require('../../lib/createNote.js');
-const {notes} = require('../../db/db.json');
+const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     let completedNotes = notes;
@@ -8,7 +8,11 @@ router.get('/notes', (req, res) => {
 })
 
 router.post('/notes', (req, res) => {
+
+    console.log(notes);
     req.body.id = notes.length.toString();
+    console.log(notes);
+
     let note = createNote(req.body, notes);
     res.json(note);
 })
--- Develop/server.js
+++ Develop/server.js
@@ -3,40 +3,49 @@ var app = express();
 const path = require('path');
 const fs = require('fs');
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 var PORT = process.env.PORT || 1000;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// here!!
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/notes',function(req,res){
-    res.sendFile(path.join(__dirname,'public/notes.html'))
-})
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/api/notes',function(req,res){
-    let noteData;
-    let readFile = fs.readFile('Develop/db/db.json', function(err, data) {
-        console.log('LINE 15 ERROR:'+err);
-        console.log('line 17 console:'+data);
-        noteData=data;
-        res.json()
-    });
+// app.get('/notes',function(req,res){
+//     res.sendFile(path.join(__dirname,'public/notes.html'))
+// })
 
-})
+// app.get('/api/notes',function(req,res){
+//     let noteData;
+//     let readFile = fs.readFile('Develop/db/db.json', function(err, data) {
+//         console.log('LINE 15 ERROR:'+err);
+//         console.log('line 17 console:'+data);
+//         noteData=data;
+//         res.json()
+//     });
 
-app.delete('/api/notes/:id',function(req,res) {
-    console.log(req.body,req.params.id)
-    let dbArray = JSON.parse('Develop/db/db.json', function(err) {console.log('line 21 error'+err)})
-    console.log(dbArray)
-})
+// })
 
-app.post('/api/notes',function(req,res) {
-    let note = req.body;
+// app.delete('/api/notes/:id',function(req,res) {
+//     console.log(req.body,req.params.id)
+//     let dbArray = JSON.parse('Develop/db/db.json', function(err) {console.log('line 21 error'+err)})
+//     console.log(dbArray)
+// })
+
+// app.post('/api/notes',function(req,res) {
+//     let note = req.body;
   
-    let dbArray;// = (fs.readFile('./Develop/db/db.json', function(err, data) {console.log(err)}));
-    console.log({note,dbArray})
-    //dbArray.push(note);
-    console.log(dbArray);
-    //fs.writeFile('Develop/db/db.json', dbArray, (err)=> console.log('line 32 error'+err))
-})
+//     let dbArray;// = (fs.readFile('./Develop/db/db.json', function(err, data) {console.log(err)}));
+//     console.log({note,dbArray})
+//     //dbArray.push(note);
+//     console.log(dbArray);
+//     //fs.writeFile('Develop/db/db.json', dbArray, (err)=> console.log('line 32 error'+err))
+// })
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
